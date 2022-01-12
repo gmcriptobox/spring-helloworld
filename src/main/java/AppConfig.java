d06d9570c2e05efdd161bc0ce3eaea7cc5a282e2
Ilya Voynov <itemboxx@mail.ru>
2022-01-12 10:27:05 +0700

    commit

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,11 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name="cat")
+    @Scope(value = "prototype")
+    public Cat getHCat() {
+        Cat cat = new Cat();
+        return cat;
+    }
 }
